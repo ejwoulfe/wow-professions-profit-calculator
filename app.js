f88469098da86b32d4d07e9b12043e3e30fd1b16
Ed <edwoulfe@gmail.com>
2020-05-29 09:41:36 -0500

    mysql connection, setting up database on my hosts server. Tables setup, schema setup.

--- app.js
+++ app.js
@@ -6,25 +6,23 @@ app.use(morgan('short'));
 
 
 
-app.get("/", (request, response) => {
-    console.log("Responding to root route");
-    response.send("Hello from Root!")
-})
-app.get("/users", (request, response) => {
-    const user1 = {
-        firstName: "Ed",
-        lastName: "Woulfe"
-    }
-    const user2 = {
-        firstName: "James",
-        lastName: "Test"
-    }
-    response.json({
-        user1,
-        user2
-    })
-})
-// localhost:3003
-app.listen(3003, () => {
-    console.log("Server is up and listening on 3003..")
+connection.connect(function (err) {
+    if (err) {
+        console.log("error here")
+        throw err
+
+    };
+    con.query("SELECT * FROM alchemy_recipes", function (err, result, fields) {
+        if (err) throw err;
+        console.log(result);
+        console.log("asdasdf")
+    });
 });
+
+
+
+connection.end()
+// // localhost:3003
+// app.listen(3003, () => {
+//     console.log("Server is up and listening on 3003..")
+// });
